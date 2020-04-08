119ad67e2b7983638a2b7e4b251bcc77b4ec75a6
XDarker <xiashengjianah@163.com>
2020-04-08 23:15:07 +0800

    OAth2配置

---
+++ is-server-auth/src/main/java/com/xdarker/security/server/auth/OAuth2WebSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.xdarker.security.server.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * @Created by XDarker
+ * @Description TODO
+ * @Date 2020/4/8 23:08
+ */
+@Configuration
+@EnableWebSecurity
+public class OAuth2WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+}
