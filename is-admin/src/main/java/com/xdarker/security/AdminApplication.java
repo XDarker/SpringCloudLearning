d8c8c376c84033ee0bce7581cab7e27bc3ad9b86
XDarker <xiashengjianah@163.com>
2020-05-03 22:07:06 +0800

    前端改造

---
+++ is-admin/src/main/java/com/xdarker/security/AdminApplication.java
@@ -0,0 +1,18 @@
+package com.xdarker.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Created by XDarker
+ * @Description TODO
+ * @Date 2020/5/3 21:44
+ */
+@SpringBootApplication
+public class AdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AdminApplication.class, args);
+  }
+
+}
